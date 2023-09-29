bf5ea76f8afbd4f2de092bd20730508b5eb19ba8
cevian-streamlit <146020056+cevian-streamlit@users.noreply.github.com>
2023-09-29 19:59:16 +0000

    Make system retain chat history

--- pages/1_TimeMachine_Demo.py
+++ pages/1_TimeMachine_Demo.py
@@ -97,6 +97,8 @@ def get_auto_retriever(index):
 def tm_demo():
     repos = get_repos()
 
+    #months = st.slider('How many months back to search (0=no limit)?', 0, 130, 0)
+
     if len(repos) > 0:
         repo = st.selectbox("Choose a repo", repos.keys())
     else:
@@ -121,8 +123,11 @@ def tm_demo():
     
     #storage_context = StorageContext.from_defaults(vector_store=vector_store)
     #index = VectorStoreIndex.from_documents(documents, storage_context=storage_context)
-    chat_engine = get_auto_retriever(index)
-    #chat_engine = index.as_chat_engine(chat_mode="best", similarity_top_k=20, verbose=True)
+    
+    #chat engine goes into the session to retain history
+    if "chat_engine" not in st.session_state.keys(): # Initialize the chat messages history
+        st.session_state.chat_engine = get_auto_retriever(index)
+        #st.session_state.chat_engine = index.as_chat_engine(chat_mode="best", similarity_top_k=20, verbose=True)
 
     if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
         st.session_state.messages.append({"role": "user", "content": prompt})
@@ -135,7 +140,7 @@ def tm_demo():
     if st.session_state.messages[-1]["role"] != "assistant":
         with st.chat_message("assistant"):
             with st.spinner("Thinking..."):
-                response = chat_engine.chat(prompt, function_call="query_engine_tool")
+                response = st.session_state.chat_engine.chat(prompt, function_call="query_engine_tool")
                 st.write(response.response)
                 message = {"role": "assistant", "content": response.response}
                 st.session_state.messages.append(message) # Add response to message history
